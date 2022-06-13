9158bfc117c65ccbbb077dfdc9e27e11ce2ce81e
yashbabiya <yashbabiya18@gmail.com>
2022-06-13 14:36:05 +0530

    Api Change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import useMouse from "./helpers/useMouse";
 import Category from "./pages/Category";
 import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 
 
 function App() {
+  const { x, y } = useMouse();
+  const styleHover = window.screen.availWidth>1035  ? {
+    display: "block",
+    position: "absolute",
+    top: y ,
+    left: x ,
+    pointerEvent:'none',
+    width: 400,
+  }:{
+    display:'none'
+  };
   return (
     <div className="App">
+      <div className="pointer" style={styleHover}></div>
       <Header/>
       <Routes>
         <Route exact path='/' element={<Home />}></Route>
--- src/helpers/search.js
+++ src/helpers/search.js
@@ -2,14 +2,18 @@ import axios from "axios";
 
 const config = {
     apiKey:'3c6939a029074c4eb39251db468f2e9f',
-    pageSize:10
+    page_size:10
 }
 export const loadRecents = async({page}) =>{
     const resd = await axios.request( {
         method: 'GET',
-        // url: 'https://free-news.p.rapidapi.com/v1/search',
-        url : `https://newsapi.org/v2/top-headlines`,
-        params:{language:'en',page:page,...config} ,
+        url: 'https://free-news.p.rapidapi.com/v1/search',
+        // url : `https://newsapi.org/v2/top-headlines`,
+        params:{q:'recent',lang:'en',page:page,page_size:config.page_size} ,
+        headers: {
+            'X-RapidAPI-Key': '93d3c35f0amshbd3e6efc056a387p19ca5ejsndd92da991081',
+            'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
+          }
         
         }).then((res)=>{
         
@@ -28,10 +32,13 @@ export const searchResults = async({keyWord,page}) => {
     
      const resd =  await axios.request( {
         method: 'GET',
-        // url: 'https://free-news.p.rapidapi.com/v1/search',
-        url : `https://newsapi.org/v2/everything`,
-        params: {q: keyWord,page:page,...config},
-        
+        url: 'https://free-news.p.rapidapi.com/v1/search',
+        // url : `https://newsapi.org/v2/everything`,
+        params: {q: keyWord,page:page,page_size:config.page_size},
+        headers: {
+            'X-RapidAPI-Key': '93d3c35f0amshbd3e6efc056a387p19ca5ejsndd92da991081',
+            'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
+          }
         }).then((res)=>{
         
         
@@ -48,9 +55,13 @@ export const searchResults = async({keyWord,page}) => {
     
     const resd =  await axios.request( {
        method: 'GET',
-       // url: 'https://free-news.p.rapidapi.com/v1/search',
-       url : `https://newsapi.org/v2/top-headlines`,
-       params: {category: keyWord,page:page,...config},
+       url: 'https://free-news.p.rapidapi.com/v1/search',
+    //    url : `https://newsapi.org/v2/top-headlines`,
+       params: {q: keyWord,page:page,page_size:config.page_size},
+       headers: {
+        'X-RapidAPI-Key': '93d3c35f0amshbd3e6efc056a387p19ca5ejsndd92da991081',
+        'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
+      }
        
        }).then((res)=>{
        
